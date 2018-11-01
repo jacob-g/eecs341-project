03958ff83572997efadb2428bb29dac0637804cf
Jacob G <jacob-x@jvvgindustries.com>
2018-11-01 17:15:19 +0100

    added Steven's original static pages

--- config/router.php
+++ config/router.php
@@ -6,6 +6,31 @@ $page_routes = array(
 		'content'	=> 'index.html',
 		'behavior'	=> 'index.php'
 	),
+	'/login/' => array(
+		'template'	=> 'base_template.html',
+		'content'	=> 'login_page.html',
+		'behavior'	=> 'login.php'
+	),
+	'/about/' => array(
+		'template'	=> 'base_template.html',
+		'content'	=> 'about.html',
+		'behavior'	=> 'about.php'
+	),
+	'/contact/' => array(
+		'template'	=> 'base_template.html',
+		'content'	=> 'contact.html',
+		'behavior'	=> 'about.php'
+	),
+	'/development/' => array(
+		'template'	=> 'base_template.html',
+		'content'	=> 'development.html',
+		'behavior'	=> 'about.php'
+	),
+	'/get-started/' => array(
+		'template'	=> 'base_template.html',
+		'content'	=> 'get-started.html',
+		'behavior'	=> 'about.php'
+	),
 	'/test/$param' => array(
 		'template'	=> 'base_template.html',
 		'content'	=> 'test_page.html',
---
+++ scripts/login.php
@@ -0,0 +1,2 @@
+<?php
+$page_params['page_title'] = 'Log in to EamonBB';
