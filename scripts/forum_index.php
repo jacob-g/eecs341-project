0fc8aad9a7a4af2add57c3595996ce60337fcf27
Jacob G <jacob-x@jvvgindustries.com>
2018-11-23 23:42:41 +0100

    added popularity metrics

--- scripts/forum_index.php
+++ scripts/forum_index.php
@@ -3,14 +3,14 @@ $page_params['page_title'] = '&Eacute;amonBB Forum Home';
 
 //select all forums and their associated category names that the user is able to read
 $statement = query('
-	SELECT c.ID AS category_id,c.name AS category_name,f.name AS forum_name,f.description,f.ID AS forum_id,COUNT(DISTINCT t.ID),COUNT(p.id) 
+	SELECT c.ID AS category_id,c.name AS category_name,f.name AS forum_name,f.description,f.ID AS forum_id,COUNT(DISTINCT t.ID),COUNT(p.id),FLOOR(AVG(t.num_views)) 
 	FROM 
 		(forum AS f LEFT JOIN category AS c ON c.ID=f.category_ID LEFT JOIN topic AS t ON t.forum_ID=f.ID LEFT JOIN post AS p ON p.topic_ID=t.ID
 		LEFT JOIN forum_group_permissions AS fgp ON fgp.forum_ID=f.ID AND fgp.group_ID=?)
 	WHERE fgp.view_forum=1 OR (fgp.view_forum IS NULL AND (SELECT view_forums FROM groups WHERE ID=?)=1)
 	GROUP BY f.ID
 	ORDER BY c.sort_order ASC,f.sort_order ASC', 'ii', array($user_info['group'], $user_info['group']));
-$statement->bind_result($category_id,$category_name, $forum_name, $forum_description, $forum_id, $num_topics, $num_posts);
+$statement->bind_result($category_id,$category_name, $forum_name, $forum_description, $forum_id, $num_topics, $num_posts, $popularity);
 
 $category_rows = new MultiPageElement();
 $last_category_id = -1;
@@ -35,6 +35,7 @@ while ($statement->fetch()) {
 	$forum_row->bind('forum_id', $forum_id);
 	$forum_row->bind('num_topics', $num_topics);
 	$forum_row->bind('num_posts', $num_posts);
+	$forum_row->bind('popularity', $popularity);
 	$last_forum_rows->addElement($forum_row);
 }
 if ($last_category_id != -1) {
